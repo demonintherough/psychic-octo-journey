109feec139be221d0c4899815988f736a44350bd
demonintherough <demonintherough@gmail.com>
2024-05-23 09:55:09 -0600

    Updated testing info to make ready to move over to other system

--- e2e/specs/users.test.js
+++ e2e/specs/users.test.js
@@ -3,12 +3,16 @@ jest.setTimeout(60000)
 describe('Basic authentication e2e tests', () => {
     beforeAll( async () => {
         // Set a definite size for the page viewport so view is consistent across browsers
+        await page.setViewport( {
+            width: 1366,
+            height: 768,
+            deviceScaleFactor: 1
+        } );
         await page.goto('https://www.google.com');
     } );
 
     it( 'Should be truthy', async () => {
-        expect( true ).toBeTruthy();
-        let element = (await page.$('#MV3Tnb'))
-        expect(element).toBe('About')
+        const text = await page.evaluate(() => document.body.textContent);
+        expect(text).toContain('google');
     })
 });
